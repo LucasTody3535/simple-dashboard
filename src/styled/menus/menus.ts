3bedf1377a3cfaa6a483af0674d4c1aa63ea7537
LucasTody3535 <lucas.web3535@gmail.com>
2023-01-19 14:11:29 -0300

    upt: HorizontalMenuWithThreeItems styled component
    Changed column gap to 2%
    Grid template column is now made with 32% for each column

--- src/styled/menus/menus.ts
+++ src/styled/menus/menus.ts
@@ -4,7 +4,7 @@ export const HorizontalMenuWithThreeItems = styled.nav`
  height: 100%;
  width: 100%;
  display: grid;
- column-gap: 5px;
- grid-template-columns: repeat(3, 33%);
+ column-gap: 2%;
+ grid-template-columns: repeat(3, 32%);
 `
 
